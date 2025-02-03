77d362504d906a4922b792a93c46eeae0dc7dd93
BogdanPoznyakov <160047125+BogdanPoznyakov@users.noreply.github.com>
2025-02-03 16:51:20 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const btnOpen = document.querySelector('.mobile-menu');
+
+btnOpen.addEventListener('click', () => {
+    btnOpen.classList.toggle('active')
+});
---
+++ scripts/sweaters.js
@@ -0,0 +1,131 @@
+const btnOpen = document.querySelector('.mobile-menuThird');
+
+btnOpen.addEventListener('click', () => {
+    btnOpen.classList.toggle('active')
+});
+
+//modal
+//FirstModal
+const btnOpenModal = document.querySelector('.cardFirst');
+const modalFIrst = document.querySelector('.modalScreen')
+const btnCloseModal = document.querySelector('.modalClose-btn')
+
+btnOpenModal.addEventListener('click', () => {
+    modalFIrst.classList.toggle('active')
+});
+
+btnCloseModal.addEventListener('click', () => {
+    modalFIrst.classList.remove('active')
+});
+
+//SecondModal
+
+const btnOpenModalSecond = document.querySelector('.cardSecond');
+const modalSecond = document.querySelector('.modalScreenSecond')
+const btnCloseModalSecond = document.querySelector('.modalClose-btnSecond')
+
+btnOpenModalSecond.addEventListener('click', () => {
+    modalSecond.classList.toggle('active')
+});
+
+btnCloseModalSecond.addEventListener('click', () => {
+    modalSecond.classList.remove('active')
+});
+
+//ThirdModal
+
+const btnOpenModalThird = document.querySelector('.cardThird');
+const modalThird = document.querySelector('.modalScreenThird');
+const btnCloseModalThird = document.querySelector('.modalClose-btnThird')
+
+btnOpenModalThird.addEventListener('click', () => {
+    modalThird.classList.toggle('active')
+});
+
+btnCloseModalThird.addEventListener('click', () => {
+    modalThird.classList.remove('active')
+});
+
+//FourthModal
+
+const btnOpenModalFourth = document.querySelector('.cardFourth');
+const modalFourth= document.querySelector('.modalScreenFourth');
+const btnCloseModalFourth = document.querySelector('.modalClose-btnFourth')
+
+btnOpenModalFourth.addEventListener('click', () => {
+    modalFourth.classList.toggle('active')
+});
+
+btnCloseModalFourth.addEventListener('click', () => {
+    modalFourth.classList.remove('active')
+});
+
+//FifthModal
+
+const btnOpenModalFifth = document.querySelector('.cardFifth');
+const modalFifth= document.querySelector('.modalScreenFifth');
+const btnCloseModalFifth = document.querySelector('.modalClose-btnFifth')
+
+btnOpenModalFifth.addEventListener('click', () => {
+    modalFifth.classList.toggle('active')
+});
+
+btnCloseModalFifth.addEventListener('click', () => {
+    modalFifth.classList.remove('active')
+});
+
+//SixthModal
+
+const btnOpenModalSixth = document.querySelector('.cardSixth');
+const modalSixth= document.querySelector('.modalScreenSixth');
+const btnCloseModalSixth = document.querySelector('.modalClose-btnSixth')
+
+btnOpenModalSixth.addEventListener('click', () => {
+    modalSixth.classList.toggle('active')
+});
+
+btnCloseModalSixth.addEventListener('click', () => {
+    modalSixth.classList.remove('active')
+});
+
+//modalSeventh
+
+const btnOpenModalSeventh = document.querySelector('.cardSeventh');
+const modalSeventh= document.querySelector('.modalScreenSeventh');
+const btnCloseModalSeventh = document.querySelector('.modalClose-btnSeventh')
+
+btnOpenModalSeventh.addEventListener('click', () => {
+    modalSeventh.classList.toggle('active')
+});
+
+btnCloseModalSeventh.addEventListener('click', () => {
+    modalSeventh.classList.remove('active')
+});
+
+//modalEighth
+
+const btnOpenModalEighth = document.querySelector('.cardEighth');
+const modalEighth= document.querySelector('.modalScreenEighth');
+const btnCloseModalEighth = document.querySelector('.modalClose-btnEighth')
+
+btnOpenModalEighth.addEventListener('click', () => {
+    modalEighth.classList.toggle('active')
+});
+
+btnCloseModalEighth.addEventListener('click', () => {
+    modalEighth.classList.remove('active')
+});
+
+//modalNinth
+
+const btnOpenModalNinth = document.querySelector('.cardNinth');
+const modalNinth = document.querySelector('.modalScreenNinth');
+const btnCloseModalNinth = document.querySelector('.modalClose-btnNinth')
+
+btnOpenModalNinth.addEventListener('click', () => {
+    modalNinth.classList.toggle('active')
+});
+
+btnCloseModalNinth.addEventListener('click', () => {
+    modalNinth.classList.remove('active')
+});
